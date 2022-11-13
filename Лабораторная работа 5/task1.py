da3b9d7a6332d2321d20f8c16afea35f4849693e
PeriParus <115573062+PeriParus@users.noreply.github.com>
2022-11-13 18:17:12 +0300

    Delete Лабораторная работа 5 directory

--- Лабораторная работа 5/task1.py
+++
@@ -1,4 +0,0 @@
-from pprint import pprint
-
-dict=[{'bin':bin(i),'oct':oct(i),'dec':(i),'hex':hex(i)}for i in range(16)]
-pprint(dict)
--- Лабораторная работа 5/task5.py
+++
@@ -1,9 +0,0 @@
-from random import sample
-import string
-
-def get_random_password(n=8) -> str:
-    string_ = string.digits + string.ascii_lowercase + string.ascii_uppercase
-    password=sample(string_,n)
-    return(password)
-
-print(get_random_password())
